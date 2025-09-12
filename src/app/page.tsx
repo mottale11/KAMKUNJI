5284ff4ac89f0952970ce8318d7f0c898ad6805f
Moses Mwai <mwaimoses200@gmail.com>
2025-09-12 05:50:53 +0000

    can you space the padding on the left of the page, move it more to the r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <Header />
-      <main className="flex-1 px-4">
+      <main className="flex-1 pl-8 pr-4">
         <HeroSection />
         <FeaturedCategories />
         <FlashDeals />
